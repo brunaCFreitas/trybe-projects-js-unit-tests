1d2f7a0442090405a2ab8ddbeaf7866efba78ef2
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-09-20 18:30:11 -0300

    Adiciona requisito 4

--- src/circle.js
+++ src/circle.js
@@ -25,8 +25,8 @@ const circle = (radius) => {
 
   return {
     radius,
-    area: PI * radius * radius,
-    circumference: 2 * PI * radius,
+    area: parseFloat((PI * radius * radius).toFixed(2)),
+    circumference: parseFloat((2 * PI * radius).toFixed(2)),
   };
 };
 
