6eacce5d59f291d5f8ff07bd4fb0bd3a32234ce7
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-09-20 15:17:00 -0300

    Adiciona requisito 1

--- src/average.js
+++ src/average.js
@@ -11,8 +11,25 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // adicione seu código aqui
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+  }
+  let soma = 0;
+
+  for (let index = 0; index < array.length; index += 1) {
+    soma += array[index];
+  }
+
+  let media = Math.round(soma / array.length);
+ 
+  return media;
 };
 
 module.exports = average;
